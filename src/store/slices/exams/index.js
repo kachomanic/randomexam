1dbde94aad99643ee0f715bf566bea89fae9605b
kachomanic <kachomanic@hotmail.com>
2023-05-11 13:48:34 -0600

    test3

--- src/store/slices/exams/index.js
+++ src/store/slices/exams/index.js
@@ -1,7 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
-import exam2img2 from "../../../assets/imgs/exam2-question2.jpg";
-import exam2img6 from "../../../assets/imgs/exam2-question6.jpg";
-import exam2img31 from "../../../assets/imgs/exam2-question31.jpg";
+import exam3img12 from "../../../assets/imgs/exam3-question12.jpg";
+import exam3img26 from "../../../assets/imgs/exam3-question26.jpg";
 
 export const examsSlice = createSlice({
   name: "exams",
@@ -45,172 +44,44 @@ export const fetchAllExams = () => (dispatch) => {
   const exams = [
     {
       Question:
-        "When two die are rolled, find the probability of getting a 5 and then a number less than 2?",
+        "Peter wants an average of 90 points in their 4 subjects, he already has 3 of the grades of 85, 92 and 88. What would he need to get in the last Test to average 90 points?",
       Img: "",
       Responses: [
-        "1/36",
-        "3/36",
-        "1/12",
-        "1/18",
-        "0.90",
-        "4/36",
-        "All are incorrect",
-      ],
-      Correct: [0],
-    },
-    {
-      Question:
-        "What is the equation of the line from the graph showing in the picture.",
-      Img: exam2img2,
-      Responses: [
-        "y = 3x + 2",
-        "y = -2x + 1",
-        "y = 2x + 2",
-        "y = 2x + 1",
-        "y = 5x + 2",
-        "y = 4x - 1",
-        "-2y = 2x +2",
+        "89",
+        "90",
+        "93",
+        "95",
+        "Is impossible to find the answer with the information provided.",
+        "91",
       ],
       Correct: [3],
     },
     {
       Question:
-        "In Mary borrowed $40,000 at a 7% rate for 3 years. How much will she pay in interest?",
+        "In a dealer a car costs $25,000 at 8% off. What is the price of the car after discount?",
       Img: "",
-      Responses: ["$5,100", "$9,200", "$6,400", "$7,600", "$8,400"],
-      Correct: [4],
-    },
-    {
-      Question:
-        "Joan is closing the register at the end of the day. He oversees counting all the changes. There are 129 coins counting only quarters and dimes. If Jason counts $27.00 in these coins, how many quarters(x) and dimes(y) are there?",
-      Img: "",
-      Responses: ["x=100, y=29", "x=70, y=95", "x=94, y=35", "x=150, y=15"],
-      Correct: [2],
-    },
-    {
-      Question:
-        "In a circle with center O, the radius is 8 cm. What is the length of the arc intercepted by a central angle of 60 degrees?",
-      Img: "",
-      Responses: ["4π cm", "8π/3 cm", "4π/3 cm", "16π/3 cm"],
-      Correct: [1],
-    },
-    {
-      Question:
-        "If the amount of money collected by the girls in all six classes is multiplied by 5, and the amount of money collected by the boys in all six classes is multiplied by 2, what would be the new ratio of the total amount of money collected by the girls to that of the boys? The chart shows how much money the boys and girls collected.",
-      Img: exam2img6,
-      Responses: ["215:86", "150:57", "445:172", "258:100", "150:86", "86:172"],
-      Correct: [2],
-    },
-    {
-      Question: "Divide (15x - 25x³ + 40x²) / 5x",
-      Img: "",
-      Responses: [
-        "3 + 5x² - 8x",
-        "3 - 5x² + 8x",
-        "3 + 5x² + 8x",
-        "3x + 8x - 5x²",
-        "3 + 5x + 8x²",
-        "No answer is correct",
-        "5 + 3x² + 8x",
-      ],
-      Correct: [1],
-    },
-    {
-      Question:
-        "How do you model the process of buying a car when you pay a $3,000 down payment and a monthly payment of $350. Which number represents the slope and which the y-intercept?",
-      Img: "",
-      Responses: [
-        "$3,000 represents the slope but $350 is not needed in this case",
-        "$350 represents the slope and $3,000 the y-intercept",
-        "$3,000 represents the slope and $350 the y-intercept.",
-        "$350 represents the slope but you cannot find the y intercept",
-        "The slope and Y-intercept is to model linear equations in coordinate plane NOT in finance",
-      ],
-      Correct: [2],
-    },
-    {
-      Question: "Does a line with positive slope go:",
-      Img: "",
-      Responses: ["Dawn", "Horizontal", "Up", "Vertical"],
-      Correct: [2],
-    },
-    {
-      Question: "The equation of the line y=2x -1 has:",
-      Img: "",
-      Responses: [
-        "Slope -1 and y-intercept 2",
-        "Slope -2 and y-intercept 1",
-        "Slope 2 and y-intercept -1",
-        "Slope 2 and y-intercept 1",
-      ],
-      Correct: [2],
-    },
-    {
-      Question:
-        "What is the area of the circle with r = 1.8 mi? (Remember pi = 3.14)",
-      Img: "",
-      Responses: [
-        "10.17 mi",
-        "4.17 mi²",
-        "3.14 mi²",
-        "10.17 mi²",
-        "8.17 mi²",
-        "12.17 mi²",
-        "-5.24mi²",
-      ],
+      Responses: ["$25,000", "$22,000", "$21,000", "$23,000", "$24,000"],
       Correct: [3],
     },
     {
-      Question:
-        "What words can you look at or help you determine if a problem is a Combination or Permutation?",
+      Question: "Find the Value of 2m if 3(m + 5) - 4 = 23",
       Img: "",
-      Responses: [
-        "Select a group indicate Permutation and find a sequence or choosing specific order group like places in a race or president secretary indicate Combination.",
-        "Select a group, like places in a race or president secretary indicate Combination and any other word indicate Permutation",
-        "Select a group, like places in a race or president secretary indicate Permutation and any other word indicate Combination",
-        "Select a group indicate Combination and find a sequence or choosing specific order group like places in a race or president secretary indicate Permutation.",
-      ],
-      Correct: [3],
-    },
-    {
-      Question: "What is half of 0.025?",
-      Img: "",
-      Responses: ["1/8", "0.1575", "0.755", "0.0125", "1/0.75", "0.00755"],
-      Correct: [3],
-    },
-    {
-      Question:
-        "Peter bought a pair of pants for $11.25 and 2 T-shirts for $7.35 each and 1 pair of shoes for double the total of one T-shirt and 1 pair of pants. What is the change if he pays with a $100 bill? ",
-      Img: "",
-      Responses: [
-        "$5.35 is the change",
-        "$1.50",
-        "$36.85",
-        "$1.15 is the change.",
-        "The answer is not on the options.",
-        "$27.60",
-      ],
+      Responses: ["-4", "5", "8", "2", "9", "-5"],
       Correct: [2],
     },
     {
       Question:
-        "Which the slope for the following linear equation? 2y = -2x + 1/3",
-      Img: "",
-      Responses: ["5", "-1", "3", "-3", "1/3", "-1/3", "1"],
-      Correct: [1],
-    },
-    {
-      Question: "Factor the polynomial: 16x² - 169",
+        "John has in his bank account $40,000 and he is planning to make 3 different investments. 1/5 of the money will go to the stock market, 1/4 of the total will be spend to sell online and the rest in real estate. How much money Does John will invest in Real Estate? ",
       Img: "",
       Responses: [
-        "(4x + 12)(4x - 12)",
-        "(4x + 13)(4x - 13)",
-        "(x + 13)(x - 13)",
-        "(x + 11)(x - 11)",
-        "(4x - 13)(4x - 13)",
-        "(4x + 13)(4x + 13)",
+        "$7,500",
+        "$6,500",
+        "$6,000",
+        "$22,000",
+        "$21,500",
+        "$13,800",
       ],
-      Correct: [1],
+      Correct: [3],
     },
     {
       Question: "The following equation y = 2x - 1 is written in:",
@@ -225,56 +96,88 @@ export const fetchAllExams = () => (dispatch) => {
     },
     {
       Question:
-        "In a school there are 350 students, 1/5 of them are Christians. From the Christians 1/10 does not go to church every week. How many students go to church every week? ",
+        "If we add Combination (7,7) with Permutation (7,7) what is the answer?",
       Img: "",
-      Responses: ["58", "24", "35", "55", "63", "No answer is correct"],
-      Correct: [4],
+      Responses: ["25", "721", "5,040", "5,041", "1025", "1024"],
+      Correct: [3],
     },
     {
-      Question: "-5² + (-5)² + 4²",
+      Question: "What is half of 0.025?",
       Img: "",
-      Responses: ["-25", "25", "-5", "5", "1", "0", "16", "64"],
-      Correct: [6],
+      Responses: ["1/8", "0.1575", "0.755", "0.0125", "1/0.75", "0.00755"],
+      Correct: [3],
     },
     {
-      Question: "If 3(5m - 1 ) = 12 ; What is the value of -2m",
+      Question: "What is Combination (5, 4)?",
       Img: "",
-      Responses: ["m = 2", "m = -3", "m = 4", "m = -2", "m = -5", "m = 0"],
+      Responses: ["24", "6", "2", "5", "10", "120", "1", "0"],
       Correct: [3],
     },
     {
       Question:
-        "There are 20 products in the box, of which 3 are faulty. Find the probability of pulling out a nonfaulty product",
+        "What is the area of a parallelogram with base 4ft and high 100 in",
       Img: "",
-      Responses: ["0", "1/10", "2/15", "1/20", "1/9", "17/20"],
-      Correct: [5],
-    },
-    {
-      Question: "Multiply xy²z<sup>4</sup> (2x² - 5yz)",
-      Img: "",
-      Responses: [
-        "2x³y²z<sup>4</sup - 5xy³z<sup>5</sup",
-        "4x³y²z<sup>4</sup> - 5xy³z<sup>5</sup>",
-        "2x³y²z<sup>5</sup> - 5xy³z<sup>5</sup>",
-        "2x³y²z<sup>4</sup> + 5xy³z<sup>5</sup>",
-        "2x³y²z<sup>4</sup> + 5xy²z<sup>5</sup>",
-        "No answer is correct",
-      ],
-      Correct: [0],
+      Responses: ["4800 ft²", "2400 ft²", "100 ft²", "4800 in²", "4800 in"],
+      Correct: [3],
     },
     {
       Question:
-        "Solve: 5(5p - 5) = 5 (Find all correct answer if there are more than one) ",
+        "A circular park has a diameter of 3 miles, and you want to jog around the park. How many miles do you complete in 4 laps? ",
       Img: "",
       Responses: [
-        "p = 1/5",
-        "p = 1.325",
-        "p = 1.425",
-        "p = 1.2",
-        "p = 6/4",
-        "p = 6/5",
+        "9.68 mi²",
+        "37.68 mi",
+        "9.68 mi",
+        "37.68 mi²",
+        "25.68 mi",
+        "17.68 in",
+        "27.68 in",
       ],
-      Correct: [3, 5],
+      Correct: [1],
+    },
+    {
+      Question: "Write the following equation in Standard Form: y = 6x + 2/3",
+      Img: "",
+      Responses: [
+        "y - 2/3 = 6x",
+        "3y - 18x = 2",
+        "- 6x - 2/3 = -y",
+        "No answer is correct",
+        "There is no standard form when you work with Fractions",
+      ],
+      Correct: [1],
+    },
+    {
+      Question:
+        "Which of the following equation is NOT the equation of the line in the Graph",
+      Img: exam3img12,
+      Responses: [
+        "2y + 4x = -1",
+        "y = - 2x -1",
+        "2y = - 4x -1",
+        "y = - 2x -1/2",
+        "2y + 1 = - 4x",
+      ],
+      Correct: [1],
+    },
+    {
+      Question: "Which of the following Graph has slope undefine? ",
+      Img: "",
+      Responses: ["y = x", "y = 4", "y = -4x", "x = -2", "y = -x", "y = -1/2"],
+      Correct: [3],
+    },
+    {
+      Question:
+        "How do you model the process of buying a car when you pay a $3,000 down payment and a monthly payment of $350. Which number represents the slope and which the y-intercept?",
+      Img: "",
+      Responses: [
+        "$3,000 represents the slope but $350 is not needed in this case",
+        "$350 represents the slope and $3,000 the y-intercept",
+        "$3,000 represents the slope and $350 the y-intercept",
+        "$350 represents the slope but you cannot find the y intercept",
+        "The slope and Y-intercept is to model linear equations in coordinate plane NOT in finance",
+      ],
+      Correct: [2],
     },
     {
       Question:
@@ -290,96 +193,181 @@ export const fetchAllExams = () => (dispatch) => {
       Correct: [3],
     },
     {
-      Question: "What is the slope of the following equation? 4y = 2x - 8",
+      Question: "Find the Value of -3P. 3(P + 5) - 4 = 23",
       Img: "",
-      Responses: ["m = 0", "m = 4", "m = -2", "m = 8", "m = 0", "m = 1/2"],
+      Responses: ["-4", "5", "-12", "2", "9", "-5"],
+      Correct: [2],
+    },
+    {
+      Question:
+        "Find the answer: (3g<sup>4</sup> + 3g² - 8 + 19g)+ (8g² - 3g<sup>4</sup> - 17)",
+      Img: "",
+      Responses: [
+        "6g<sup>4</sup> + 11g² + 25g - 19",
+        "6g<sup>4</sup> + 11g² + 19g + 25",
+        "3g<sup>4</sup> + 11g² + 19g - 25",
+        "6g<sup>4</sup> + 11g² + 19g - 25",
+        "6g<sup>4</sup> - 11g² + 19g - 25",
+        "No answers is correct",
+      ],
+      Correct: [5],
+    },
+    {
+      Question: "Divide (48m - 30n³ + 16t²) / 4",
+      Img: "",
+      Responses: [
+        "12m - 15/2n³ + 5t²",
+        "12m - 15/2n² + 4t²",
+        "12m - 13/2n³ + 4t²",
+        "10m - 15/2n³ + 4t²",
+        "12m - 15/2n³ + 4t²",
+      ],
+      Correct: [4],
+    },
+    {
+      Question: "4,500 is 25% of What number?",
+      Img: "",
+      Responses: ["13,500", "20,000", "18,500", "19,000", "17,000", "18,000"],
       Correct: [5],
     },
     {
       Question:
-        "Which are the intercepts from the following Equations: 2x + 3y =9",
+        "Find the probability of selecting a black marker if you have 8 black, 2 red and 5 blue",
+      Img: "",
+      Responses: ["7/15", "8/20", "8/8", "8/15", "8/12"],
+      Correct: [3],
+    },
+    {
+      Question: "Find the answer: (h² + 3h - 5) - (7h - 3h² -12)",
       Img: "",
       Responses: [
-        "(0, 1) and (4.5, 2)",
-        "(0, -3) and (-4.5, 0)",
-        "(0, -3) and (4.5, 2)",
-        "(0, 3) and (4.5, 0)",
-        "No answer is correct",
-        "(1, 1) and (-4.5, 2)",
+        "4h² + 4h + 7",
+        "4h² - 4h - 17",
+        "3h² - 4h + 7",
+        "4h² - 4h + 7",
+        "7h² - 4h + 12",
       ],
       Correct: [3],
     },
     {
-      Question:
-        "What is the equation of the line passes through the points (1, 2) and (2, -2)",
+      Question: "What is the unit that we use to represent volume?",
       Img: "",
       Responses: [
-        "y = 4/3x + 1",
-        "y = -4x + 6",
-        "y = 4x + 4",
-        "y = 4x -2",
-        "y = 6x -1",
-      ],
-      Correct: [1],
-    },
-    {
-      Question: "Convert the following measure: 0.0015 km to cm",
-      Img: "",
-      Responses: ["50 cm", "15 cm", "0.15 cm", "150 cm", "50 cm", "1,500 cm"],
-      Correct: [3],
-    },
-    {
-      Question:
-        "What words can you look at or help you determine if a problem is a Combination or Permutation?",
-      Img: "",
-      Responses: [
-        "Select a group indicate Permutation and find a sequence or choosing specific order group like places in a race or president secretary indicate Combination. ",
-        "Select a group, like places in a race or president secretary indicate Combination and any other word indicate Permutation. ",
-        "Select a group, like places in a race or president secretary indicate Permutation and any other word indicate Combination. ",
-        "Select a group indicate Combination and find a sequence or choosing specific order group like places in a race or president secretary indicate Permutation.",
-      ],
-      Correct: [3],
-    },
-    {
-      Question:
-        "What is the meaning and range of the following set of Data? 20, 10, 10, 15, 25, 20, 30, 30.",
-      Img: "",
-      Responses: [
-        "mean 20 and range 20",
-        "mean 10 and range 20",
-        "mean 10 and range 15",
-        "mean 15 and range 20",
-        "mean 15 and range 15",
+        "cube units",
+        "square units",
+        "linear units",
+        "none of these",
+        "all of these",
       ],
       Correct: [0],
     },
     {
-      Question: "Find the area of the following triangle",
-      Img: exam2img31,
+      Question:
+        "A Gym charges one time enrolment fee of $50 plus $12 monthly fee after that. What equation best model the information describes? ",
+      Img: "",
       Responses: [
-        "90 u²",
-        "27 u²",
-        "54 u²",
-        "36 u²",
-        "42 u²",
-        "The answer is not in the options",
+        "y = 50x",
+        "y = 12x",
+        "y = 12x + 50",
+        "y = 50x + 12",
+        "None is correct",
       ],
       Correct: [2],
     },
     {
       Question:
-        "Which of the following equation does NOT represent a function?",
+        "Which of the following equations has y-intercept equal to zero?",
       Img: "",
       Responses: [
-        "y + 2 = x³ + 2",
-        "x² = y + 1",
-        "x = 2",
-        "y - x = -2",
-        "y = 1/2x³",
-        "-y + 1 = -x",
-        "y = 5",
+        "-y = 1/2 x - 1 ",
+        "y = x - 1",
+        "y = 1/2 x + 2",
+        "y = 1/2 x -7",
+        "y -2 = 1/2 x - 2",
+      ],
+      Correct: [4],
+    },
+    {
+      Question:
+        "What is the area of a triangle that has base 5 and height is double the base?",
+      Img: "",
+      Responses: ["15", "20", "50", "25", "40", "35"],
+      Correct: [3],
+    },
+    {
+      Question:
+        "What is the equation of the line from the graph showing in the picture",
+      Img: exam3img26,
+      Responses: [
+        "y = 3x + 2",
+        "y = -2x + 1",
+        "y = 2x + 2",
+        "y = 2x + 1",
+        "y = 5x + 2",
+        "y = 4x - 1",
+        "-2y = 2x + 2",
       ],
       Correct: [3],
+    },
+    {
+      Question:
+        "Do I find the perimeter of the square with the same formula as a rectangle",
+      Img: "",
+      Responses: [
+        "Maybe if they have the same perimeter",
+        "You can calculate both using the formula of the rectangle",
+        "The formula of the square you can use for a rectangle",
+      ],
+      Correct: [1],
+    },
+    {
+      Question: "What is the perimeter of an square that has a side of 25m?",
+      Img: "",
+      Responses: ["90", "75", "40", "50", "100", "75"],
+      Correct: [4],
+    },
+    {
+      Question:
+        "In a lock of 3 character's combination (2 of them are numbers and one is a letter) there are 10 numbers to choose from (0,1,2,3,4,5,6,7,8,9) and 24 letters. How many different options for a code do we have if: Numbers cannot be repeated",
+      Img: "",
+      Responses: [
+        "600",
+        "6",
+        "720",
+        "5040",
+        "2160",
+        "1024",
+        "2400",
+        "Not enough information to answer",
+      ],
+      Correct: [4],
+    },
+    {
+      Question:
+        "What is the mode from the following set of Data? 20, 10, 10, 15, 25, 20, 30, 10",
+      Img: "",
+      Responses: [
+        "mode = 10",
+        "mode = 15",
+        "mode = 20",
+        "mode = 25",
+        "mode = 30",
+      ],
+      Correct: [4],
+    },
+    {
+      Question:
+        "How many solutions does the following equation have? 2x² + 15x + 7 = 0",
+      Img: "",
+      Responses: ["1", "2", "0", "4", "3"],
+      Correct: [1],
+    },
+    {
+      Question:
+        "Which of the following values for c makes a trinomial a perfect square? 4x² + 20x + c = 0",
+      Img: "",
+      Responses: ["c = 49", "c = 20", "c = 25", "c = 36", "c = 80"],
+      Correct: [2],
     },
   ];
 
