ed150071a7f1279407c1047fcaf549a6849079a2
Rubens <ruben.norori@ooqia.com>
2023-11-05 10:58:46 -0600

    test8

--- src/store/slices/exams/index.js
+++ src/store/slices/exams/index.js
@@ -42,90 +42,62 @@ export const fetchAllExams = () => (dispatch) => {
   const exams = [
     {
       Question:
-        "What is the area of a triangle that has base 5 and height is double the base? ",
+        "What is the area of a triangle that has base 4 and height is 7?",
       Img: "",
-      Responses: ["15", "20", "50", "25", "35", "40"],
+      Responses: ["15", "12", "20", "14", "21", "28"],
       Correct: [3],
     },
     {
       Question:
-        "What is the area of the triangle with base = 14in and high = 8in Impossible to find the answer.",
+        "What is the area of the triangle in square feet with base = 24in and high = 2ft ;   (1ft = 12in)",
       Img: "",
-      Responses: ["80 in²", "56 in", "120 in²", "112 in²", "56 in²", "110 in"],
+      Responses: ["12 ft²", "8 ft²", "24 ft²", "4 ft²", "2 ft²", "6 ft²"],
       Correct: [4],
     },
     {
-      Question: "How to find the perimeter?",
+      Question: "What is the perimeter of a square that has a side of 10m?",
       Img: "",
-      Responses: [
-        "Adding all sides around the figure",
-        "Adding all sides that are the same",
-        "Adding the most important information",
-        "Not sure how to find it",
-        "Adding the first 3 sides and multiply the last one",
-      ],
+      Responses: ["10", "16", "30", "40", "28", "14"],
+      Correct: [3],
+    },
+    {
+      Question: "What is 100% of 10?",
+      Img: "",
+      Responses: ["10", "1", "1000", "100", "5", "20"],
       Correct: [0],
     },
     {
       Question:
-        "Do I find the perimeter of the square with the same formula than a rectangle",
+        "What is the area of the circle with r = 10 m? (Remember pi = 3.14)",
       Img: "",
       Responses: [
-        "Maybe if they have the same perimeter",
-        "You can calculate both using the formula of the rectangle",
-        "The formula of the square you can use it for a rectangle",
-        "Only if the rectangle has one 90 degrees angle",
-      ],
-      Correct: [1],
-    },
-    {
-      Question:
-        "What is the area of the circle with r = 1.8 mi ? (remember pi = 3.14)",
-      Img: "",
-      Responses: [
-        "10.85 mi",
-        "4.17 mi²",
-        "3.14 mi²",
-        "10.17 mi²",
-        "8.17 mi²",
-        "12.17 mi²",
-        "-5.24 mi²",
+        "10.85 m²",
+        "4.17 m²",
+        "3.14 m²",
+        "314 m²",
+        "8.17 m²",
+        "12.17 m²",
       ],
       Correct: [3],
     },
     {
       Question:
-        "What is an area of a rectangle with side A = 20ft and B = 8ft?",
+        "What is an area of a rectangle with side A = 10ft and B = 8ft?",
       Img: "",
-      Responses: [
-        "120 ft",
-        "80 ft²",
-        "120 ft²",
-        "110 ft²",
-        "200 ft²",
-        "160 ft²",
-      ],
+      Responses: ["100 ft", "80 ft²", "60 ft²", "110 ft²", "70 ft²", "80 ft²"],
       Correct: [5],
     },
     {
-      Question: "What is the perimeter of a square that has a side of 25m?",
+      Question: "What is the perimeter of a square that has a side of 13mm?",
       Img: "",
-      Responses: ["90", "75", "40", "50", "100", "75"],
+      Responses: ["60", "75", "40", "50", "52", "75"],
       Correct: [4],
     },
     {
-      Question:
-        "A circular park has a diameter of 3 miles, and you want to jog around the park. How many miles do you complete in 4 laps?",
+      Question: "What is 50% of 50? ",
       Img: "",
-      Responses: [
-        "9.68 mi²",
-        "37.68 mi",
-        "9.68 mi",
-        "37.18 mi²",
-        "25.68 mi",
-        "97.68 mi",
-      ],
-      Correct: [1],
+      Responses: ["25", "12.5", "500", "250", "5", "50"],
+      Correct: [0],
     },
     {
       Question:
@@ -136,46 +108,60 @@ export const fetchAllExams = () => (dispatch) => {
     },
     {
       Question:
+        "What is the area of the parallelogram with base = 12ft and hight = 12ft",
+      Img: "",
+      Responses: ["102 ft", "96 ft", "144 ft²", "136 ft²", "24 ft²", "21 ft²"],
+      Correct: [2],
+    },
+    {
+      Question:
         "What is the difference between a rectangle and a parallelogram?",
       Img: "",
       Responses: [
-        "One has opposite sides different and the other not.",
-        "The difference is in the formulas for Perimeters and areas.",
-        "The difference is that all sides are different.",
-        "The sides of the rectangles intersect with 90 degrees angels and sides of the parallelogram no.",
-        "There is no difference between them.",
+        "One has opposite sides different and the other not",
+        "The difference is in the formulas for Perimeters and areas",
+        "The difference is that all sides are different",
+        "The sides of the rectangles intersect with 90 degrees angels and sides of the parallelogram no",
+        "There is no difference between them",
       ],
       Correct: [3],
     },
     {
-      Question: "Convert the following measure: 0.0015 km to cm",
+      Question: "Convert the following measure: 0.0015 km to mm",
       Img: "",
-      Responses: ["50 cm", "15 cm", "0.15 cm", "150 cm", "50 cm", "1,500 cm"],
+      Responses: [
+        "500 mm",
+        "5150 mm",
+        "0.95 mm",
+        "1500 mm",
+        "2500 mm",
+        "15000 mm",
+      ],
       Correct: [3],
     },
     {
       Question:
-        "If a rectangle has a width of 2 ft and a length of 3 ft. What is the area in square inches?",
+        "If a rectangle has a width of 2 km and a length of 3 km. What is the area in square km? ",
       Img: "",
-      Responses: ["1564 in²", "164 in", "514 in²", "72 in²", "864 in²"],
+      Responses: ["10 km²", "16 km²", "14 km²", "12 km²", "6 km²"],
       Correct: [4],
     },
     {
       Question:
-        "Convert the following measure: 1.12 miles to yards if (1 mi = 1760 yd)",
+        "Convert the following measure: 1.82 miles to yards if (1 mi = 1760 yd)",
       Img: "",
       Responses: [
         "3971.2 yd",
         "2971.2 yd",
-        "871.2 yd",
+        "1871.2 yd",
+        "3203.2 yd",
         "1971.2 yd",
-        "971.2 yd",
-        "1771.2 yd",
+        "2771.2 yd",
       ],
       Correct: [3],
     },
     {
-      Question: "Find the area of a circle with diameter = 8 in.",
+      Question: "Find the area of a circle with diameter = 8 in",
       Img: "",
       Responses: [
         "50.24 ft",
@@ -189,33 +175,33 @@ export const fetchAllExams = () => (dispatch) => {
     },
     {
       Question:
-        "What is the area of a parallelogram with base 4ft and high 100 in",
+        "What is the area of a parallelogram with base 12in and high 100 in",
       Img: "",
-      Responses: ["4800 ft²", "2400 in²", "100 ft²", "4800 in²", "4800 in"],
+      Responses: ["1800 in²", "1400 in²", "100 in²", "1200 in²", "4800 in²"],
       Correct: [3],
     },
     {
-      Question: "Which of the following is equal to -5?",
+      Question: "Which of the following is equal to -6?",
       Img: "",
       Responses: [
-        "-5 + 5",
-        "4 + 1",
-        "3 - 8",
-        "10 - 5",
-        "-5 + 1",
-        "None is equal to -5",
+        "-5 + 8",
+        "3 + 1",
+        "2 - 8",
+        "12 - 5",
+        "-8 + 1",
+        "None is equal to -6",
       ],
       Correct: [2],
     },
     {
-      Question: "Find the answer: -5 + 7 * 2 -10 =",
+      Question: "Find the answer: 8 + 7 * 12 - 40 =",
       Img: "",
-      Responses: ["13", "-1", "-14", "2", "-8", "-15"],
+      Responses: ["55", "52", "-44", "60", "-50", "-15"],
       Correct: [1],
     },
     {
       Question:
-        "When we calculate using Order of Operation the first operation if is present is always:",
+        "When we calculate using Order of Operation the second operation if there is present is always:",
       Img: "",
       Responses: [
         "Multiplication",
@@ -225,21 +211,21 @@ export const fetchAllExams = () => (dispatch) => {
         "Subtraction",
         "Exponent",
       ],
-      Correct: [3],
+      Correct: [5],
     },
     {
-      Question: "Find the answer: -2 + 9 * 3 -10 +3 * (-10) =",
+      Question: "Find the answer: - 12 + 9 * 3 - 15 + 3 * (5) =",
       Img: "",
-      Responses: ["4", "-20", "20", "-15", "9", "-10", "18"],
+      Responses: ["14", "20", "16", "15", "10", "18"],
       Correct: [3],
     },
     {
       Question:
-        "Talking about US money, what is the equivalent of $3.75 in quarters?",
+        "Talking about US money, what is the equivalent of $5.25 in quarters?",
       Img: "",
       Responses: [
         "6 quarters",
-        "15 quarters",
+        "21 quarters",
         "12 quarters",
         "75 quarters",
         "10 quarters",
@@ -249,49 +235,35 @@ export const fetchAllExams = () => (dispatch) => {
     },
     {
       Question:
-        "Talking about US money, what is the equivalent of $5.40 in dimes?",
+        "Talking about US money, what is the equivalent of $12.10 in dimes?",
       Img: "",
       Responses: [
-        "36 dimes",
-        "54 dimes",
-        "48 dimes",
+        "136 dimes",
         "65 dimes",
-        "44 dimes",
-        "40 dimes",
+        "121 dimes",
+        "124 dimes",
+        "118 dimes",
+        "110 dimes",
       ],
-      Correct: [1],
+      Correct: [2],
     },
     {
-      Question: "Find the value of X if 10X = 30",
+      Question: "Find the value of K if 10K = 440",
       Img: "",
-      Responses: ["4", "10", "2", "3", "5", "300", "1/3"],
+      Responses: ["54", "10", "32", "44", "25", "300"],
       Correct: [3],
     },
     {
-      Question: "Find the value of X if -4X = 28",
+      Question: "Find the value of X if -4X = 52",
       Img: "",
-      Responses: ["-6", "-10", "2", "-7", "6", "-4"],
+      Responses: ["-6", "-10", "12", "-13", "-16", "-4"],
       Correct: [3],
     },
     {
-      Question: "Multiply the following numbers: 0.25 x 0.1 =",
+      Question: "Multiply the following numbers: 0.25 x 0.5 = ",
       Img: "",
-      Responses: ["0.025", "2.5", "25", "0.125", "0.4", "0.0025"],
-      Correct: [0],
-    },
-    {
-      Question:
-        "Which of the following expressions is equal (same answer) to 2 x ½",
-      Img: "",
-      Responses: [
-        "4 times 10",
-        "8 divided by 8",
-        "12 minus 10",
-        "7 times ½",
-        "7 plus 6",
-        "4 multiply 4",
-      ],
-      Correct: [1],
+      Responses: ["0.025", "1.5", "12", "0.125", "0.14", "0.1025"],
+      Correct: [3],
     },
   ];
 
